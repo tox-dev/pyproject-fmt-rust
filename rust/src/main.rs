55f28130461bdd1381bca27b23901dc6787dcfca
Philipp A. <flying-sheep@web.de>
2024-07-31 21:18:43 +0200

    Fix arrays being expanded without reason (#43)
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -63,16 +63,16 @@ pub fn format_toml(content: &str, opt: &Settings) -> String {
     reorder_tables(&root_ast, &mut tables);
 
     let options = Options {
-        align_entries: false,           // do not align by =
-        align_comments: true,           // align inline comments
-        align_single_comments: true,    // align comments after entries
-        array_trailing_comma: true,     // ensure arrays finish with trailing comma
-        array_auto_expand: true,        // arrays go to multi line for easier diffs
-        array_auto_collapse: false,     // do not collapse for easier diffs
-        compact_arrays: false,          // do not compact for easier diffs
-        compact_inline_tables: false,   // do not compact for easier diffs
-        compact_entries: false,         // do not compact for easier diffs
-        column_width: opt.column_width, // always expand arrays per https://github.com/tamasfe/taplo/issues/390
+        align_entries: false,         // do not align by =
+        align_comments: true,         // align inline comments
+        align_single_comments: true,  // align comments after entries
+        array_trailing_comma: true,   // ensure arrays finish with trailing comma
+        array_auto_expand: true,      // arrays go to multi line when too long
+        array_auto_collapse: false,   // do not collapse for easier diffs
+        compact_arrays: false,        // leave whitespace
+        compact_inline_tables: false, // leave whitespace
+        compact_entries: false,       // leave whitespace
+        column_width: opt.column_width,
         indent_tables: false,
         indent_entries: false,
         inline_table_expand: true,
@@ -312,6 +312,49 @@ mod tests {
         };
         let got = format_toml(start.as_str(), &settings);
         let expected = read_to_string(data.join("ruff-order.expected.toml")).unwrap();
+        assert_eq!(got, expected);
+        let second = format_toml(got.as_str(), &settings);
+        assert_eq!(second, got);
+    }
+
+    /// Test that the column width is respected,
+    /// and that arrays are neither exploded nor collapsed without reason
+    #[rstest]
+    fn test_column_width() {
+        let start = indoc! {r#"
+        [build-system]
+        build-backend = "backend"
+        requires = ["c>=1.5", "d == 2" ]
+
+        [project]
+        name = "beta"
+        dependencies = [
+        "e>=1.5",
+        ]
+        "#};
+        let settings = Settings {
+            column_width: 80,
+            indent: 4,
+            keep_full_version: false,
+            max_supported_python: (3, 12),
+            min_supported_python: (3, 12),
+        };
+        let got = format_toml(start, &settings);
+        let expected = indoc! {r#"
+        [build-system]
+        build-backend = "backend"
+        requires = [ "c>=1.5", "d==2" ]
+
+        [project]
+        name = "beta"
+        classifiers = [
+            "Programming Language :: Python :: 3 :: Only",
+            "Programming Language :: Python :: 3.12",
+        ]
+        dependencies = [
+            "e>=1.5",
+        ]
+        "#};
         assert_eq!(got, expected);
         let second = format_toml(got.as_str(), &settings);
         assert_eq!(second, got);
--- tests/test_main.py
+++ tests/test_main.py
@@ -2,26 +2,73 @@ from __future__ import annotations
 
 from textwrap import dedent
 
+import pytest
+
 from pyproject_fmt_rust import Settings, format_toml
 
 
-def test_format_toml() -> None:
-    txt = """
-    [project]
-    keywords = [
-      "A",
-    ]
-    classifiers = [
-      "Programming Language :: Python :: 3 :: Only",
-    ]
-    dynamic = [
-      "B",
-    ]
-    dependencies = [
-      "requests>=2.0",
-    ]
-    """
-
+@pytest.mark.parametrize(
+    ("start", "expected"),
+    [
+        pytest.param(
+            """
+            [project]
+            keywords = [
+              "A",
+            ]
+            classifiers = [
+              "Programming Language :: Python :: 3 :: Only",
+            ]
+            dynamic = [
+              "B",
+            ]
+            dependencies = [
+              "requests>=2.0",
+            ]
+            """,
+            """\
+            [project]
+            keywords = [
+                "A",
+            ]
+            classifiers = [
+                "Programming Language :: Python :: 3 :: Only",
+                "Programming Language :: Python :: 3.7",
+                "Programming Language :: Python :: 3.8",
+            ]
+            dynamic = [
+                "B",
+            ]
+            dependencies = [
+                "requests>=2.0",
+            ]
+            """,
+            id="expanded",
+        ),
+        pytest.param(
+            """
+            [project]
+            keywords = ["A"]
+            classifiers = ["Programming Language :: Python :: 3 :: Only"]
+            dynamic = ["B"]
+            dependencies = ["requests>=2.0"]
+            """,
+            """\
+            [project]
+            keywords = [ "A" ]
+            classifiers = [
+                "Programming Language :: Python :: 3 :: Only",
+                "Programming Language :: Python :: 3.7",
+                "Programming Language :: Python :: 3.8",
+            ]
+            dynamic = [ "B" ]
+            dependencies = [ "requests>=2.0" ]
+            """,
+            id="collapsed",
+        ),
+    ],
+)
+def test_format_toml(start: str, expected: str) -> None:
     settings = Settings(
         column_width=120,
         indent=4,
@@ -29,23 +76,5 @@ def test_format_toml() -> None:
         min_supported_python=(3, 7),
         max_supported_python=(3, 8),
     )
-    res = format_toml(dedent(txt), settings)
-
-    expected = """\
-    [project]
-    keywords = [
-        "A",
-    ]
-    classifiers = [
-        "Programming Language :: Python :: 3 :: Only",
-        "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: 3.8",
-    ]
-    dynamic = [
-        "B",
-    ]
-    dependencies = [
-        "requests>=2.0",
-    ]
-    """
+    res = format_toml(dedent(start), settings)
     assert res == dedent(expected)
